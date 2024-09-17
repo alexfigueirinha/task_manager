5377cad65170b63fb346aefa22a9c5872b79c977
alexfigueirinha <alex.figueirinha@sp.senai.br>
2024-09-17 10:13:54 -0300

    adicionado validações e mensagens de erros e sucess

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -18,14 +19,17 @@ class TaskController extends Controller
         return view('tasks.create');
     }
 
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
         $task = new Task();
         $task->title = $request->title;
         $task->description = $request->description;
         $task->save();
 
-        return redirect()->route('tasks.index');
+        // Definir uma mensagem de sucesso
+        return redirect()->route('tasks.index')->with('success', 'Tarefa criada com sucesso!');
+        
+        //return redirect()->route('tasks.index');
     }
 
     public function edit($id)
@@ -34,14 +38,17 @@ class TaskController extends Controller
         return view('tasks.edit', compact('task'));
     }
 
-    public function update(Request $request, $id)
+    public function update(StoreTaskRequest $request, $id)
     {
         $task = Task::findOrFail($id);
         $task->title = $request->title;
         $task->description = $request->description;
         $task->save();
 
-        return redirect()->route('tasks.index');
+        // Definir uma mensagem de sucesso
+        return redirect()->route('tasks.index')->with('success', 'Tarefa atualizada com sucesso!');
+
+        //return redirect()->route('tasks.index');
     }
 
     public function destroy($id)
